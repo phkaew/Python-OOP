8018c74c81311487c125a87ae8fd997984709a95
phanomyong kaewprachum <phkaew@gmail.com>
2022-06-21 13:33:57 +0700

    insert date

--- 01 - Introduction to OOP/main.py
+++ 01 - Introduction to OOP/main.py
@@ -1,5 +1,15 @@
 # How to create a class:
 class Item:
+    def __init__(self,name:str,price:float,quantity=0):
+      # run validations to the received arguments
+        assert price >=0
+        assert quantity >=0
+
+
+       #Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity= quantity
     def calculate_total_price(self, x, y):
         return x * y
 
@@ -7,9 +17,9 @@ class Item:
 item1 = Item()
 
 # Assign attributes:
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+# item1.name = "Phone"
+# item1.price = 100
+# item1.quantity = 5
 
 # Calling methods from instances of a class:
 print(item1.calculate_total_price(item1.price, item1.quantity))
@@ -18,9 +28,9 @@ print(item1.calculate_total_price(item1.price, item1.quantity))
 item2 = Item()
 
 # Assign attributes
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
+# item2.name = "Laptop"
+# item2.price = 1000
+# item2.quantity = 3
 
 # Calling methods from instances of a class: 
 print(item2.calculate_total_price(item2.price, item2.quantity))
